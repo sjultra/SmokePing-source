455cd41ff89dbb2dc46c2cd18504a68584df8897
alex <alex@SJU-Alexs-2019-MacBook-Pro-15inch.local>
2023-09-12 15:38:58 -0700

    fix smokeping build from source

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+service apache2 start
+service apache2 status
+/etc/init.d/apache2 reload
+
+/usr/local/smokeping/bin/smokeping --config /usr/local/smokeping/etc/config --nodaemon --debug-daemon
